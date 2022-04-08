b8448fda2c97c7109e6edabade190eef2ea92bea
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-08 23:57:16 +0600

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_control.urls')),
     path('appointments/', include('appointment_control.urls')),
+    path('articles/', include('article_control.urls')),
+    path('emergency/', include('emergency_service_control.urls')),
+    path('patient-community/', include('patient_community_control.urls')),
+    path('health-advisor/', include('health_advisor_control.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
