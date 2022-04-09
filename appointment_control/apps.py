5ff3e77e561d031af6cb0b2a080a309f9de96198
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-09 14:15:09 +0600

    Update apps.py

--- appointment_control/apps.py
+++ appointment_control/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class AppointmentControlConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'appointment_control'
+    verbose_name = 'Appointment & Prescription Section'
