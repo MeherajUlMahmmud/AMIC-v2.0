3bb9e01efe0b71a176508699b3ff16c133a6ed56
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-09 14:15:12 +0600

    Update apps.py

--- article_control/apps.py
+++ article_control/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ArticleControlConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'article_control'
+    verbose_name = 'Article Section'
