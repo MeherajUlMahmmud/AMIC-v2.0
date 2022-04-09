9af7a3dc42287302a18fdb5caaab9c29f677007a
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-09 14:16:43 +0600

    Create ambulance_views.py

---
+++ emergency_service_control/views/ambulance_views.py
@@ -0,0 +1,11 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import render, redirect
+
+from emergency_service_control.models import AmbulanceModel
+
+def ambulance_home_view(request):
+	pass
+
+
+def ambulance_detail_view(request, pk):
+	pass
