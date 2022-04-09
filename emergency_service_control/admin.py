36179fd2f4684a7d00493896039010df3ef8edf6
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-09 14:16:33 +0600

    admin

--- emergency_service_control/admin.py
+++ emergency_service_control/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(BloodRequestModel)
+admin.site.register(PlasmaRequestModel)
+admin.site.register(AmbulanceModel)
