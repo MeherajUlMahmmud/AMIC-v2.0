4da87ddbc9f08da088d2ed7a3e4b46cfcce880a7
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-14 02:24:07 +0600

    Update forms.py

--- ot_control/forms.py
+++ ot_control/forms.py
@@ -7,6 +7,10 @@ class OTScheduleForm(ModelForm):
 	"""
 	This form is used to create a new OT Schedule.
 	"""
+	start_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+	end_time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+	date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+	
 	class Meta:
 		model = OTScheduleModel
 		fields = "__all__"
