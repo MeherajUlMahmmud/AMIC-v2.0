80e5b9ab8ddc0e2de946e22f5342af4ca62b2b3d
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-17 13:50:04 +0600

    ot schedule complete

--- ot_control/admin.py
+++ ot_control/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import OTScheduleModel
 
-admin.site.register(OTScheduleModel)
+class OTScheduleModelAdmin(admin.ModelAdmin):
+	list_display = ('id', 'patient', 'date', 'start_time', 'end_time', 'status')
+	list_filter = ('date', 'start_time', 'end_time', 'status')
+	search_fields = ('patient', 'date', 'start_time', 'end_time', 'status')
+
+admin.site.register(OTScheduleModel, OTScheduleModelAdmin)
--- ot_control/views.py
+++ ot_control/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import redirect, render
 from ot_control.forms import OTScheduleForm
 
 from ot_control.models import OTScheduleModel
-from user_control.models import PatientModel
+from user_control.models import PatientModel, UserModel
 
 
 def ot_booking_home_view(request):
-	bookings = OTScheduleModel.objects.filter(date__gte=datetime.date.today()).order_by('date') # get all bookings within the next 7 days
+	bookings = OTScheduleModel.objects.filter(date__range=[datetime.date.today(), datetime.date.today() + datetime.timedelta(days=7)]).order_by('date') # get all bookings within the next 7 days
+
 	context = {
 		'bookings': bookings,
 	}
@@ -25,7 +26,7 @@ def ot_booking_post_view(request):
 			patient = PatientModel.objects.get(user=user)
 			booking.patient = patient
 			booking.save()
-			return redirect('ot-booking-home')
+			return redirect('ot-booking-detail', pk=booking.id)
 	context = {
 		'task': task,
 		'form': form,
@@ -33,21 +34,69 @@ def ot_booking_post_view(request):
 	return render(request, 'pages/ot/ot_booking_create_update.html', context)
 
 
-def update_ot_bookng_view(request):
-	return render(request, 'pages/ot/ot_booking_create_update.html')
+def update_ot_bookng_view(request, pk):
+	task = "Update Operation Theater Booking"
+	booking = OTScheduleModel.objects.get(id=pk)
+
+	# check if the user is the owner of the booking
+	if request.user != booking.patient.user: # if the user is not the owner of the booking
+		return redirect('ot-booking-detail', pk) # redirect to detail page
+	
+	form = OTScheduleForm(instance=booking)
+	if request.method == 'POST':
+		form = OTScheduleForm(request.POST, instance=booking)
+		if form.is_valid():
+			form.save()
+			return redirect('ot-booking-detail', pk)
+	context = {
+		'task': task,
+		'form': form,
+	}
+	return render(request, 'pages/ot/ot_booking_create_update.html', context)
 
 
-def delete_ot_booking_view(request):
-	return render(request, 'pages/ot/ot_booking_delete.html')
+def delete_ot_booking_view(request, pk):
+	booking = OTScheduleModel.objects.get(id=pk)
+
+	# check if the user is the owner of the booking
+	if request.user != booking.patient.user: # if the user is not the owner of the booking
+		return redirect('ot-booking-detail', pk) # redirect to detail page
+	
+	if request.method == 'POST':
+		booking.delete()
+		return redirect('ot-booking-home')
+
+	context = {
+		'booking': booking,
+	}
+	return render(request, 'pages/ot/ot_booking_delete.html', context)
 
 
 def ot_booking_detail_view(request, pk):
-	booking = OTScheduleModel.objects.get(pk=pk)
+	booking = OTScheduleModel.objects.get(id=pk)
+	
+	# check if the user is the owner of the booking
+	my_booking = False
+	if request.user == booking.patient.user:
+		my_booking = True
+
+	# check if the booking is in the future
+	is_pending = False
+	if booking.status == 'Pending':
+		is_pending = True
 	context = {
 		'booking': booking,
+		'my_booking': my_booking,
+		'is_pending': is_pending,
 	}
 	return render(request, 'pages/ot/ot_booking_detail.html', context)
 
 
-def users_ot_booking_view(request):
-	return render(request, 'pages/ot/users_ot_booking.html')
+def users_ot_booking_view(request, pk):
+	user = UserModel.objects.get(id=pk)
+	patient = PatientModel.objects.get(user=user) # get patient
+	bookings = OTScheduleModel.objects.filter(patient=patient).order_by('date') # get all bookings for the patient
+	context = {
+		'bookings': bookings,
+	}
+	return render(request, 'pages/ot/users_ot_booking.html', context)
