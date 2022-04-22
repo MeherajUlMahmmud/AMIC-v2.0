e946cac20184ba260e372b7c153262bec1eb65c2
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-22 13:55:44 +0600

    Update views.py

--- pathology_control/views.py
+++ pathology_control/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
 
-# Create your views here.
+from pathology_control.models import TestModel
+
+
+def pathology_home_view(request):
+    bookings = TestModel.objects.filter(user=request.user).order_by("--created_at")
+
+    pending_bookings = bookings.filter(status="pending")
+    completed_bookings = bookings.filter(status="completed")
+    in_progress_bookings = bookings.filter(status="in progress")
+    rejected_bookings = bookings.filter(status="rejected")
+    context = {
+        "pending_bookings": pending_bookings,
+        "completed_bookings": completed_bookings,
+        "in_progress_bookings": in_progress_bookings,
+        "rejected_bookings": rejected_bookings,
+    }
+    return render(request, "pages/pathology/pathology_home.html", context)
+
+
+def pathology_post_view(request):
+    return render(request, "pages/pathology/pathology_post.html")
+
+
+def pathology_detail_view(request):
+    return render(request, "pages/pathology/pathology_detail.html")
+
+
+def update_pathology_view(request):
+    return render(request, "pages/pathology/update_pathology.html")
+
+
+def delete_pathology_view(request):
+    return render(request, "pages/pathology/delete_pathology.html")
