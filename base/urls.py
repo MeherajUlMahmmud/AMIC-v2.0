6368700a19fe7b965788868d6a506b034841a3ad
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-22 13:54:06 +0600

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -5,15 +5,16 @@ from django.urls import path, include
 from base import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('user_control.urls')),
-    path('appointments/', include('appointment_control.urls')),
-    path('articles/', include('article_control.urls')),
-    path('emergency/', include('emergency_service_control.urls')),
-    path('health-advisor/', include('health_advisor_control.urls')),
-    path('ot-bookings/', include('ot_control.urls')),
-    path('patient-community/', include('patient_community_control.urls')),
-    path('pharmacy/', include('pharmacy_control.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("user_control.urls")),
+    path("appointments/", include("appointment_control.urls")),
+    path("articles/", include("article_control.urls")),
+    path("emergency/", include("emergency_service_control.urls")),
+    path("health-advisor/", include("health_advisor_control.urls")),
+    path("ot-bookings/", include("ot_control.urls")),
+    path("pathology/", include("pathology_control.urls")),
+    path("patient-community/", include("patient_community_control.urls")),
+    path("pharmacy/", include("pharmacy_control.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
