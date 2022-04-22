62a82468204a2e46016aa52b60046f8a44fade7f
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-22 13:54:33 +0600

    Update models.py

--- ot_control/models.py
+++ ot_control/models.py
@@ -1,32 +1,26 @@
 from django.db import models
-
+from user_control.constants import *
 from user_control.models import PatientModel
 
 
 class OTScheduleModel(models.Model):
-	"""
-	Model for OT Schedule
-	"""
-	STATUS_CHOICES = [
-		('Pending', 'Pending'),
-		('Confirmed', 'Confirmed'),
-		('Cancelled', 'Cancelled'),
-		('In Progress', 'In Progress'),
-		('Completed', 'Completed'),
-	]
-	patient = models.ForeignKey(PatientModel, on_delete=models.CASCADE)
-	name = models.CharField(max_length=100)
-	description = models.TextField(null=True, blank=True)
-	start_time = models.TimeField()
-	end_time = models.TimeField()
-	date = models.DateField()
-	status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='Pending')
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-	
-	def __str__(self):
-		return self.name
-	
-	class Meta:
-		verbose_name = 'OT Schedule'
-		verbose_name_plural = 'OT Schedules'
+    """
+    Model for OT Schedule
+    """
+
+    patient = models.ForeignKey(PatientModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    date = models.DateField()
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="Pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "OT Schedule"
+        verbose_name_plural = "OT Schedules"
