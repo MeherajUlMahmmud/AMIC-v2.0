1c60fe687b74304b93ade70eb46bb6bb8a8c8987
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-22 13:55:04 +0600

    Update models.py

--- pathology_control/models.py
+++ pathology_control/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from user_control.constants import *
+from user_control.models import UserModel
 
-# Create your models here.
+
+class TestTypeModel(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class TestModel(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    test_type = models.ForeignKey(TestTypeModel, on_delete=models.CASCADE)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default="P")
+    date = models.DateField()
+    patient_name = models.CharField(max_length=100)
+    gender = models.CharField(
+        max_length=10, choices=GENDER_CHOICES, null=True, blank=True
+    )
+    blood_group = models.CharField(
+        max_length=10, choices=BLOOD_GROUP_CHOICES, null=True, blank=True
+    )
+    date_of_birth = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
