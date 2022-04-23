e9930b8a019116ed3606d3870c868004e3324f21
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-24 03:27:46 +0600

    Update urls.py

--- pathology_control/urls.py
+++ pathology_control/urls.py
@@ -3,21 +3,21 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", pathology_home_view, name="ot-booking-home"),  # path for home page
-    path("book", pathology_post_view, name="ot-booking-post"),  # path for post request
+    path("", pathology_home_view, name="pathology-home"),  # path for home page
+    path("book", pathology_post_view, name="pathology-post"),  # path for post request
     path(
         "update-booking/<int:pk>",
         update_pathology_view,
-        name="pathology-booking-update",
+        name="pathology-update",
     ),  # path for update request
     path(
         "delete-booking/<int:pk>",
         delete_pathology_view,
-        name="pathology-booking-delete",
+        name="pathology-delete",
     ),  # path for delete request
     path(
         "booking-detail/<int:pk>",
         pathology_detail_view,
-        name="pathology-booking-detail",
+        name="pathology-detail",
     ),  # path for request detail
 ]
