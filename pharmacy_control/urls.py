75031e816585b78053c86a6ccf550c0251139683
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-24 18:57:14 +0600

    Update urls.py

--- pharmacy_control/urls.py
+++ pharmacy_control/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', pharmacy_home_view, name='pharmacy-home'),
+    path("", pharmacy_home_view, name="pharmacy-home"),
+    path(
+        "medicine/<int:medicine_id>", pharmacy_medicine_view, name="pharmacy-medicine"
+    ),
 ]
