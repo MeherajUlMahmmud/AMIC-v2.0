1e3b52610eec1b4e20c3d9502bb3e94b70e06724
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-25 15:18:37 +0600

    Update views.py

--- covid_control/views.py
+++ covid_control/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def covid_home_view(request):
+    return render(request, "pages/covid/covid_home.html")
+
+
+def covid_post_view(request):
+    return render(request, "pages/covid/covid_post_update.html")
+
+
+def covid_update_view(request):
+    return render(request, "pages/covid/covid_post_update.html")
+
+
+def covid_delete_view(request):
+    return render(request, "pages/covid/covid_delete.html")
+
+
+def covid_detail_view(request):
+    return render(request, "pages/covid/covid_detail.html")
