eaabe6794064ba4c2f961d47925ed5e9e986c120
Meheraj <57069206+MeherajUlMahmmud@users.noreply.github.com>
2022-04-25 15:13:08 +0600

    Update admin.py

--- pharmacy_control/admin.py
+++ pharmacy_control/admin.py
@@ -38,9 +38,30 @@ class MedicineCartItemModelAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 
+class MedicineOrderModelAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "status",
+        "payment_method",
+        "total_items",
+        "total_price",
+        "created_at",
+    ]
+    list_display_links = ["user"]
+    search_fields = ["user", "status", "payment_method"]
+    list_per_page = 25
+
+
+class MedicineOrderItemModelAdmin(admin.ModelAdmin):
+    list_display = ["medicine_order", "medicine", "quantity", "price"]
+    list_display_links = ["medicine"]
+    search_fields = ["medicine"]
+    list_per_page = 25
+
+
 admin.site.register(MedicineSectionModel, MedicineSectionModelAdmin)
 admin.site.register(MedicineModel, MedicineModelAdmin)
 admin.site.register(MedicineCartModel, MedicineCartModelAdmin)
 admin.site.register(MedicineCartItemModel, MedicineCartItemModelAdmin)
-admin.site.register(MedicineOrderModel)
-admin.site.register(MedicineOrderItemModel)
+admin.site.register(MedicineOrderModel, MedicineOrderModelAdmin)
+admin.site.register(MedicineOrderItemModel, MedicineOrderItemModelAdmin)
